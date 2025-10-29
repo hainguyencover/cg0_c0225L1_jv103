33c1b07993b679b3946fb5d4898beeff02415593
hainguyencover <duchain916@gmail.com>
2025-10-29 17:18:56 +0700

    Hoàn thành lesson14&15_spring_security

---
+++ lesson14&15_spring_security/wbds-2025-spring-security-authorization/src/main/java/com/example/wbds2025springsecurityauthorization/security/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.example.wbds2025springsecurityauthorization.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
